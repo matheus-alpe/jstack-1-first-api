c8f2f54210a3f15f59e1bf1f1ebea0fa031d5f24
Matheus Pereira <matttalves@gmail.com>
2022-06-17 12:05:59 -0300

    feat: queryParams

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 const http = require('http');
+const { URL } = require('url');
 
 const routes = require('./routes')
 
 const server = http.createServer((request, response) => {
-    console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+    const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+
+    console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
 
     const route = routes.find((routeObject) => (
-        routeObject.endpoint === request.url && routeObject.method === request.method
+        routeObject.endpoint === parsedUrl.pathname && routeObject.method === request.method
     ));
 
     if (!route) {
         response.writeHead(404, { 'content-type': 'text/html' });
-        response.end(`Cannot ${request.method} ${request.url}`);
+        response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
         return;
     }
 
+    request.query = Object.fromEntries(parsedUrl.searchParams);
     route.handler(request, response);
 });
 
