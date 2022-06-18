4522d10bd843962c53b8c0df79821b0f554e6d5c
Matheus Pereira <matttalves@gmail.com>
2022-06-17 23:12:27 -0300

    feat: data on body and post route

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,7 +3,7 @@ const users = require('../mocks/users');
 module.exports = {
     listUsers(request, response) {
         const { order } = request.query;
-        const sortedUsers = users.sort((a, b) => {
+        const sortedUsers = [ ...users ].sort((a, b) => {
             if (order === 'desc') {
                 return a.id < b.id ? 1 : -1;
             }
@@ -23,5 +23,27 @@ module.exports = {
         }
 
         response.send(200, user);
+    },
+
+    createUser(request, response) {
+        let body = '';
+
+        request.on('data', (chunk) => {
+            body += chunk;
+        });
+
+        request.on('end', () => {
+            body = JSON.parse(body);
+
+            const lastUserId = users[users.length - 1].id;
+            const newUser = {
+                id: lastUserId + 1,
+                name: body.name
+            }
+
+            users.push(newUser);
+            response.send(200, newUser);
+        });
+
     }
 };
